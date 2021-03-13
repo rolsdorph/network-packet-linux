4d3d0c65b1dff5ac754618fce1596e41b4974db9
Mads Rolsdorph <m.rolsdorph@gmail.com>
2021-03-13 15:50:46 +0000

    Use ip instead of ifconfig to create test interface

--- setup_unixtest_interface.sh
+++ setup_unixtest_interface.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sudo ip link add haskellnetwork type dummy
-sudo ifconfig haskellnetwork hw ether 12:34:56:78:37
+sudo ip link set haskellnetwork address 12:34:56:78:37:00
 sudo ip link set haskellnetwork up
